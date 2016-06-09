39a5bf48dc62016e735d1c865f03bbd8900aa222
Matthew Sullivan <hayduke19us@gmail.com>
2016-06-09 03:19:00 -0500

    Remove einstein and twain. This is a Hitchens memorial

--- chris_quotes.rb
+++ chris_quotes.rb
@@ -3,37 +3,69 @@ require 'active_support'
 require "Nokogiri"
 require "open-uri"
 
-puts "What quote would you prefer Hitchens, Twain, or Einstein?"
-answer = gets.chomp 
-answer_down = answer.downcase
-hitchens = 'http://www.patheos.com/blogs/unreasonablefaith/2011/12/the-20-best-christopher-hitchens-quotes/'
-twain = 'http://jpetrie.myweb.uga.edu/clemens.html'
-einstein = 'http://rescomp.stanford.edu/~cheshire/EinsteinQuotes.html' 
 
-if answer_down =~ /h.*|i.*/
-  doc = Nokogiri::HTML(open(hitchens))
-  blockquotes = doc.css('blockquote')
-  random_content =  blockquotes[(rand(30))].content
-  puts "Your Hitchens quote of the day, god rest his soal:"
-elsif answer_down =~ /t.*|w.*/
-  doc = Nokogiri::HTML(open(twain))
-  p = doc.css('p')
-  random_content = p[(rand(30))].content
-  puts "Your " +  answer.capitalize + " quote of the day:"
-elsif answer_down =~ /e.*|i.*/
-  doc = Nokogiri::HTML(open(einstein))
-  line = doc.css('li') 
-  random_content = line[(rand(30))].content
-  puts "Your " +  answer.capitalize + " quote of the day:"
-else
-  puts " Only three choices for now you quote lush"
+class Quote
+  attr_accessor :author, :index
+  attr_reader :document, :quotes
+
+  def initialize(author='hitchens')
+    @author   = author
+    @document = Nokogiri::HTML(open endpoints[@author])
+    @quotes   = @document.css('blockquote')
+    @index    = rand(@quotes&.count - 1)
+  end
+
+  def get
+    quote = quotes[index]
+
+    if quote
+      puts ""
+      puts success_message
+      quote.content
+    else
+      find_quote!
+    end
+  end
+
+  def find_quote!
+    quote = nil
+
+    while quote.nil? || index > 0
+      if quote = document[index]
+        puts success_message
+        quote.content
+      else
+        self.index -= 1
+      end
+    end
+  end
+
+  def endpoints
+    {
+      'hitchens' => 'http://www.patheos.com/blogs/unreasonablefaith/2011/12/the-20-best-christopher-hitchens-quotes/',
+      'einstein' => 'http://rescomp.stanford.edu/~cheshire/EinsteinQuotes.html'
+    }
+  end
+
+  private
+
+  def success_message
+    "Your #{author.capitalize} quote of the day."
+  end
 end
 
+quote = Quote.new.get
 
-puts '=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='
-if answer = answer
-  puts random_content.to_s.ljust(100)
+puts "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n"
+br = 8
+words = quote.to_s.split(" ")
+words = words.map do |word|
+  if words.index(word) >= br
+    br += 8
+    "#{word}\n\n"
+  else
+    word
+  end
 end
-puts '=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
 
-
+puts " " + words.join(" ")
